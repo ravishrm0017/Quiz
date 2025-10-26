92287deea1f1603bba9858762a513cc26cb49c99
ravishrm0017 <ravishrm0017@gmail.com>
2025-10-26 11:51:14 +0530

    Refactor checkbox assignments in tiktac.py
    Removed duplicate checkbox assignments and added two new checkboxes.

--- tiktac.py
+++ tiktac.py
@@ -1,6 +1,6 @@
 import streamlit as st
 box1 = st.checkbox("")
 box2 = st.checkbox("")
-box2 = st.checkbox("")
-box2 = st.checkbox("")
+box3 = st.checkbox("")
+box4 = st.checkbox("")
 st.checkbox("")
